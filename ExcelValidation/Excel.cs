6ad9dec9ab78b536049611f1be3af86755cdf514
seaweed78 <adhikarikshitiz12@gmail.com>
2023-10-02 17:49:34 +0545

    first commit

---
+++ ExcelValidation/Excel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.Office.Interop.Excel;
+using Workbook = Microsoft.Office.Interop.Excel.Workbook;
+using Worksheet = Microsoft.Office.Interop.Excel.Worksheet;
+
+namespace ExcelValidation
+{
+    public class Excel
+    {
+        string path = "";
+        Application excel = new Application();
+        private Workbook wb;
+        private Worksheet ws;
+        HashSet<string> emailHashSet = new HashSet<string>();
+        public Excel(string path, int sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCellsFromColumn(int i, int j)
+        {
+            //i++;
+            //j++;
+
+            if (ws.Cells[i, j].Value2 != null)
+            {
+                return ws.Cells[i, j].Value2;
+            }
+            else
+            {
+                return " ";
+            }
+        }
+
+        public void WriteInCell(int i, int j, string value)
+        {
+
+            ws.Cells[i, j].Value = value;
+        }
+
+        public void SaveWorkBook()
+        {
+            wb.Save();
+            wb.Close();
+        }
+
+        public void CloseFile()
+        {
+            wb.Close();
+        }
+    }
+}
---
+++ ExcelValidation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace ExcelValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> emailHashSet = new HashSet<string>();
+            string[] result = new string[11];
+            Excel baseFile = new Excel(@"D:\DotNet\ExcelValidation\ExcelFile2.xlsx", 1);
+            Excel newFile = new Excel(@"D:\DotNet\ExcelValidation\Test1.xlsx", 1);
+
+            for (int i = 1; i <= 1; i++)
+            {
+                for (int j = 2; j < 10; j++)
+                {
+                    result[j] = baseFile.ReadCellsFromColumn(j, i);
+                    if (emailHashSet.Contains(result[j]))
+                    {
+                        newFile.WriteInCell(j, i, result[j]);
+
+                        newFile.WriteInCell(j, i+2, "duplicate");
+                    }
+                    else
+                    {
+                        emailHashSet.Add(result[j]);
+                        newFile.WriteInCell(j,i, result[j]);
+                        newFile.WriteInCell(j, i+2, "valid");
+                    }
+                }
+            }
+            newFile.SaveWorkBook();
+            baseFile.CloseFile();
+            foreach (var email in emailHashSet)
+            {
+                Console.WriteLine(email);
+            }
+            Console.ReadLine();
+        }
+    }
+}
