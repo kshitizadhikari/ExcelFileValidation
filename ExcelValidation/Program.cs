659f0f0a8ffe772176dade8f46599cce33abc770
seaweed78 <adhikarikshitiz12@gmail.com>
2023-10-03 13:46:41 +0545

    UserInputFilePath

--- ExcelValidation/Program.cs
+++ ExcelValidation/Program.cs
@@ -10,8 +10,10 @@ namespace ExcelValidation
         {
             HashSet<string> dbEmailHashSet = new HashSet<string>();
             List<string> excelEmailList = new List<string>();
-            Excel baseFile = new Excel(@"D:\DotNet\ExcelValidation\abc.xlsx", 1);
-
+            int count = 0;
+            int emailColumn = 1;
+            //Excel baseFile = new Excel(@"D:\DotNet\ExcelValidation\abc.xlsx", 1);
+            char choice;
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Server=DESKTOP-72AIJKL;Initial Catalog=EmailCheckDb;Integrated Security=True";
             SqlConnection connection = new SqlConnection(connectionString);
 
@@ -19,7 +21,6 @@ namespace ExcelValidation
             {
                 connection.Open();
                 Console.WriteLine("Connection established\n");
-
                 string selectQuery = "SELECT email FROM Users"; // Adjust your SQL query as needed
                 SqlCommand command = new SqlCommand(selectQuery, connection);
                 SqlDataReader reader = command.ExecuteReader();
@@ -27,37 +28,69 @@ namespace ExcelValidation
                 while (reader.Read())
                 {
                     string email = reader["email"].ToString();
+                    count++;
                     dbEmailHashSet.Add(email);
                 }
                 reader.Close();
 
-                for (int i = 1; i <= 1; i++)
+                Console.WriteLine("Select the appropriate file format : \n");
+                Console.WriteLine("Excel File => 1");
+                Console.WriteLine("CSV File => 2");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine("\nPress Enter");
+                Console.ReadLine();
+
+                switch (choice)
                 {
-                    for (int j = 2; j < 10; j++)
-                    {
-                        string cellValue = baseFile.ReadCellsFromColumn(j, i).ToString();
-                        if (dbEmailHashSet.Contains(cellValue))
+                    case '1':
+
+                        Console.WriteLine("Please enter the path or the filename that you want to use : ");
+                        string filePath = Console.ReadLine();
+
+                        if (System.IO.File.Exists(filePath))
                         {
-                            baseFile.WriteInCell(j, i + 2, "duplicate");
-                            excelEmailList.Add(cellValue);
+                            Console.WriteLine($"Selected File => {filePath}");
+                            Excel baseFile = new Excel(filePath, 1);
+                            for (int i = 1; i <= count; i++)
+                            {
+
+                                string cellValue = baseFile.ReadCellsFromColumn(i, emailColumn);
+                                if (dbEmailHashSet.Contains(cellValue))
+                                {
+                                    baseFile.WriteInCell(i, emailColumn + 2, "duplicate");
+                                    excelEmailList.Add(cellValue);
+                                }
+                            }
+
+                            baseFile.SaveWorkBook();
+
+                            Console.WriteLine("Emails in Database :");
+                            foreach (var email in dbEmailHashSet)
+                            {
+                                Console.WriteLine(email);
+                            }
+
+                            Console.WriteLine("\nDuplicate Emails in Excel File :");
+                            foreach (var email in excelEmailList)
+                            {
+                                Console.WriteLine(email);
+                            }
                         }
-                    }
+                        else
+                        {
+                            Console.WriteLine("The specified file path does not exist.");
+                        }
+
+                        Console.ReadLine();
+                        break;
+
+                    case '2':
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
                 }
-
-                baseFile.SaveWorkBook();
-
-                Console.WriteLine("Emails in Database :");
-                foreach (var email in dbEmailHashSet)
-                {
-                    Console.WriteLine(email);
-                }
-
-                Console.WriteLine("\nDuplicate Emails in Excel File :");
-                foreach (var email in excelEmailList)
-                {
-                    Console.WriteLine(email);
-                }
-
             }
             catch (SqlException ex)
             {
